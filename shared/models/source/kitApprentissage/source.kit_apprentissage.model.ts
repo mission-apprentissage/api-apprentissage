2a4164b076361cdfc3a5e38e68daf102612659a9
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-02-20 09:21:20 +0100

    feat: job d'import des données Kit Apprentissage

---
+++ shared/models/source/kitApprentissage/source.kit_apprentissage.model.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+import { IModelDescriptor, zObjectId } from "../../common";
+
+const collectionName = "source.kit_apprentissage" as const;
+
+const indexes: IModelDescriptor["indexes"] = [[{ date: 1, source: 1 }, {}]];
+
+export const zKitApprentissageData = z
+  .object({
+    "Code Diplôme": z.string().nullable(),
+    "Intitulé diplôme (DEPP)": z.string().nullable(),
+    FicheRNCP: z.string().nullable(),
+    "Niveau fiche RNCP": z.string().nullable(),
+    "Abrégé de diplôme (RNCP)": z.string().nullable(),
+    "Dernière MaJ": z.string().nullable(),
+    "Accès à l'apprentissage de la fiche RNCP (oui/non)": z.string().nullable(),
+    "Date d'échéance de la fiche RNCP": z.string().nullable(),
+    "Fiche ACTIVE/INACTIVE": z.string().nullable(),
+  })
+  .strict();
+
+export const zKitApprentissage = z
+  .object({
+    _id: zObjectId,
+    source: z.literal("kit_apprentissage_20240119.csv"),
+    date: z.date(),
+    data: zKitApprentissageData,
+  })
+  .strict();
+
+export const sourceKitApprentissageModelDescriptor = {
+  zod: zKitApprentissage,
+  indexes,
+  collectionName,
+} as const satisfies IModelDescriptor;
+
+export type ISourceKitApprentissage = z.output<typeof zKitApprentissage>;

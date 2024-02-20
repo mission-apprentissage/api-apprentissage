7bc55afc5557c63077acc0ec13b96ec2451eccb0
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-02-20 16:15:16 +0100

    chore: fix typecheck

--- shared/models/models.ts
+++ shared/models/models.ts
@@ -34,5 +34,4 @@ export type IDocumentMap = {
   "source.bcn": ISourceBcn;
   "source.kit_apprentissage": ISourceKitApprentissage;
   "source.catalogue": ISourceCatalogue;
-  "source.kit_apprentissage": ISourceKitApprentissage;
 };

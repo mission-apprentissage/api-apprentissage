d94f08e75653057f3df8d999864eecc8cfb50c07
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-03-19 06:57:17 +0100

    fix: index conflict

--- server/src/common/errors/withCause.ts
+++ server/src/common/errors/withCause.ts
@@ -1,4 +1,7 @@
+import { captureException } from "@sentry/node";
+
 export function withCause<T extends Error>(error: T, cause: Error): T {
   error.cause = cause;
+  captureException(cause);
   return error;
 }

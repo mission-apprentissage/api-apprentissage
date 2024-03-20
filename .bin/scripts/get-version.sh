8f79767b98fc7339d6d505075a1508647822a30a
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-03-20 06:21:38 +0100

    chore: fix deploy action (#47)

--- .bin/scripts/get-version.sh
+++ .bin/scripts/get-version.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# If REQUEST_VERSION is set, and different from latest tag, use it
-if [[ -n "${REQUEST_VERSION:-}" ]]; then
-  if [[ "${REQUEST_VERSION}" != "latest" ]]; then
-    echo "${REQUEST_VERSION}"
-    exit 0
-  fi
-fi
-
 VERSION=$(git describe --tags --abbrev=0 --candidates 100 --always)
 HEAD=$(git rev-parse HEAD)
 

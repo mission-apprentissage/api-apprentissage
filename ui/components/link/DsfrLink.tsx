1ff08183adaea1934832ee1db731f20544389db3
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-05-13 15:06:40 +0200

    feat: open external link in new tab

--- ui/components/link/DsfrLink.tsx
+++ ui/components/link/DsfrLink.tsx
@@ -1,20 +1,34 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { Box, Link } from "@mui/material";
 import NextLink, { LinkProps } from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
+
+import { publicConfig } from "@/config.public";
 
 export function DsfrLink({
   children,
   noArrow = false,
   ...props
 }: { children: ReactNode; noArrow?: boolean } & LinkProps) {
+  const { href, ...rest } = props;
+
+  const isExternal = useMemo(() => {
+    if (typeof href !== "string") return false;
+    const url = new URL(href, publicConfig.baseUrl);
+    if (url.protocol !== "http:" && url.protocol !== "https:") return false;
+    return new URL(href, publicConfig.baseUrl).hostname !== publicConfig.host;
+  }, [href]);
+
   return (
     <Link
       component={NextLink}
       sx={{
         textUnderlinePosition: "under",
       }}
-      {...props}
+      href={href}
+      rel={isExternal ? "noopener noreferrer" : undefined}
+      target={isExternal ? "_blank" : undefined}
+      {...rest}
     >
       {children}
       {!noArrow && (

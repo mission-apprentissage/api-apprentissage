f6ae8a75a33bdeb99e2c2c84fda7a8ba0a4f607b
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-05-13 15:13:04 +0200

    fix: normalize certification data tags

--- shared/docs/certification/00_identifiant/identifiant/index.ts
+++ shared/docs/certification/00_identifiant/identifiant/index.ts
@@ -8,7 +8,7 @@ export const identifiantField: DocField = {
   description,
   information,
   sample: "exemple : La certification correspond au couple CFD 50022137 - RNCP37537",
-  tags: ["identifiant.cfd", "identifiant.rncp", "identifiant.rncp_anterieur_2019"],
+  tags: [".cfd", ".rncp", ".rncp_anterieur_2019"],
   tip: {
     title: "structure des codes CFD et RNCP",
     content: tip,

1b7560fa07e813d673657114705c2275ad2a4d61
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-07-25 14:29:28 +0200

    chore: fix action

--- .bin/scripts/sentry-release.sh
+++ .bin/scripts/sentry-release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-VERSION=$(git rev-parse --short HEAD)
+VERSION=${1:?"Veuillez préciser la version à build"}
+
+COMMIT_ID=$(git rev-parse --short HEAD)
+PREV_COMMIT_ID=$(git rev-parse --short HEAD^1)
 
 if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE:-}" ]]; then
   ansible_extra_opts+=("--vault-password-file" "${SCRIPT_DIR}/get-vault-password-client.sh")
@@ -22,4 +25,4 @@ docker run \
   -e SENTRY_AUTH_TOKEN="${SENTRY_AUTH_TOKEN}" \
   -e SENTRY_DSN="${SENTRY_DSN}" \
   ghcr.io/mission-apprentissage/mna_${PRODUCT_NAME}_server:${VERSION} \
-  /app/server/sentry-release-server.sh "mission-apprentissage/${REPO_NAME}" "${VERSION}" 
+  /app/server/sentry-release-server.sh "mission-apprentissage/${REPO_NAME}" "${COMMIT_ID}" "${PREV_COMMIT_ID}"

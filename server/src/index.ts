46d3c82acacc8974482df5f31eabed365414df02
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-09-07 10:42:38 +0200

    feat: add api token certificates (#150)
    * feat: add api token certificates
    
    * chore: fix tests
    
    * chore: fix test port number

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,7 @@
-import { captureException } from "@sentry/node";
-import { modelDescriptors } from "shared/models/models";
+import { config } from "dotenv";
 
-import { startCLI } from "./commands.js";
-import config from "./config.js";
-import { setupJobProcessor } from "./jobs/jobs.js";
-import logger from "./services/logger.js";
-import { initMailer } from "./services/mailer/mailer.js";
-import { configureDbSchemaValidation, connectToMongodb } from "./services/mongodb/mongodbService.js";
+config({ path: ".env" });
+config({ path: ".env.local", override: true });
 
-(async function () {
-  try {
-    await connectToMongodb(config.mongodb.uri);
-    await configureDbSchemaValidation(modelDescriptors);
-
-    // We need to setup even for server to be able to call addJob
-    await setupJobProcessor();
-
-    await initMailer();
-
-    await startCLI();
-  } catch (err) {
-    captureException(err);
-    logger.error({ err }, "startup error");
-    // eslint-disable-next-line n/no-process-exit
-    process.exit(1);
-  }
-})();
+// Dynamic import to start server after env are loaded
+import("./main.js");

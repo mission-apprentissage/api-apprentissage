a9b48d1a653840f2208419fe79ef75e4506ea9f1
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-09-16 13:49:51 +0200

    feat: ajout de `createOffer` & `updateOffer` dans le job module

--- sdk/src/client/job/job.module.ts
+++ sdk/src/client/job/job.module.ts
@@ -1,3 +1,5 @@
+import { zJobOfferCreateResponseLba } from "../../external/laBonneAlternance.api.js";
+import type { IJobOfferWritable } from "../../models/index.js";
 import type { IJobSearchQuery, IJobSearchResponse } from "../../routes/index.js";
 import { zJobSearchResponse } from "../../routes/job.routes.js";
 import type { ApiClient } from "../client.js";
@@ -9,6 +11,8 @@ export type JobSearchFilter = Omit<IJobSearchQuery, "romes"> & {
 
 export type JobModule = {
   search(filter: JobSearchFilter): Promise<IJobSearchResponse>;
+  createOffer(data: IJobOfferWritable): Promise<string>;
+  updateOffer(id: string, data: IJobOfferWritable): Promise<void>;
 };
 
 export function buildJobModule(apiClient: ApiClient): JobModule {
@@ -26,6 +30,13 @@ export function buildJobModule(apiClient: ApiClient): JobModule {
       const data = await apiClient.get("/job/v1/search", { querystring });
 
       return parseApiResponse(data, zJobSearchResponse);
+    },
+    createOffer: async (data: IJobOfferWritable): Promise<string> => {
+      const res = await apiClient.post("/job/v1/offer", { body: data });
+      return parseApiResponse(res, zJobOfferCreateResponseLba).id;
+    },
+    updateOffer: async (id: string, data: IJobOfferWritable): Promise<void> => {
+      await apiClient.put("/job/v1/offer/:id", { params: { id }, body: data });
     },
   };
 }

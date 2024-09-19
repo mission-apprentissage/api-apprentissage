83de958243065731bb1ef33b0fcb944f82509688
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-09-19 18:27:09 +0200

    fix: technical doc language detection

--- ui/app/documentation-technique/page.tsx
+++ ui/app/documentation-technique/page.tsx
@@ -1,11 +1,16 @@
 import "./overwride.css";
 
+import acceptLanguage from "accept-language";
 import { headers } from "next/headers";
 
 import RedocPageClient from "./components/redoc";
 
+acceptLanguage.languages(["fr", "en"]);
+
 export default function RedocPage() {
   const nonce = headers().get("x-nonce") ?? "";
 
-  return <RedocPageClient nonce={nonce} />;
+  const locale = acceptLanguage.get(headers().get("accept-language") ?? "");
+
+  return <RedocPageClient nonce={nonce} locale={locale ?? "fr"} />;
 }

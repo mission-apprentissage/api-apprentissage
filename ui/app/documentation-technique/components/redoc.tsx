4dff9468b6e45120e883a813fcdd2c2a4a54d936
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-09-19 22:37:01 +0200

    fix: technical doc language detection (#167)

--- ui/app/documentation-technique/components/redoc.tsx
+++ ui/app/documentation-technique/components/redoc.tsx
@@ -4,14 +4,14 @@ import { RedocStandalone } from "redoc";
 
 import { publicConfig } from "@/config.public";
 
-export default function RedocPageClient({ nonce }: { nonce: string }) {
+export default function RedocPageClient({ nonce, locale }: { nonce: string; locale: string }) {
   return (
     <RedocStandalone
       specUrl={`${publicConfig.apiEndpoint}/documentation/json`}
       options={{
         sortPropsAlphabetically: false,
         sortEnumValuesAlphabetically: true,
-        sortOperationsAlphabetically: true,
+        sortOperationsAlphabetically: false,
         sortTagsAlphabetically: false,
         menuToggle: true,
         hideSchemaTitles: false,
@@ -23,26 +23,29 @@ export default function RedocPageClient({ nonce }: { nonce: string }) {
             },
           },
         },
-        labels: {
-          enum: "Enum",
-          enumSingleValue: "Valeur",
-          enumArray: "Éléments",
-          default: "Défaut",
-          deprecated: "Déprécié",
-          example: "Exemple",
-          examples: "Exemples",
-          recursive: "Récurssif",
-          arrayOf: "Tableau de ",
-          webhook: "Événement",
-          const: "Valeur",
-          noResultsFound: "Aucun résultat trouvé",
-          download: "Télécharger",
-          downloadSpecification: "Télécharger la spécification OpenAPI",
-          responses: "Réponses",
-          callbackResponses: "Réponses des callbacks",
-          requestSamples: "Exemples de requêtes",
-          responseSamples: "Exemples de réponses",
-        },
+        labels:
+          locale === "fr"
+            ? {
+                enum: "Enum",
+                enumSingleValue: "Valeur",
+                enumArray: "Éléments",
+                default: "Défaut",
+                deprecated: "Déprécié",
+                example: "Exemple",
+                examples: "Exemples",
+                recursive: "Récurssif",
+                arrayOf: "Tableau de ",
+                webhook: "Événement",
+                const: "Valeur",
+                noResultsFound: "Aucun résultat trouvé",
+                download: "Télécharger",
+                downloadSpecification: "Télécharger la spécification OpenAPI",
+                responses: "Réponses",
+                callbackResponses: "Réponses des callbacks",
+                requestSamples: "Exemples de requêtes",
+                responseSamples: "Exemples de réponses",
+              }
+            : {},
         nonce,
       }}
       onLoaded={(...args) => {

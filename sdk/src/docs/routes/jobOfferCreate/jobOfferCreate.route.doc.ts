b95c4d8972f52fed74bf38d8e603d10e5e12fedf
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-09-21 22:34:37 +0200

    feat: add addOperationDoc (#173)

--- sdk/src/docs/routes/jobOfferCreate/jobOfferCreate.route.doc.ts
+++ sdk/src/docs/routes/jobOfferCreate/jobOfferCreate.route.doc.ts
@@ -6,9 +6,12 @@ export const jobOfferCreateRouteDoc = {
     "This API allows you to maximize the visibility of your apprenticeship job offers by sharing them with the La bonne alternance service, which then automatically redistributes them to the most relevant candidates on its site as well as its partner sites.",
   response: {
     description: "Success",
-    _: {
-      id: {
-        description: "Identifier of the created offer",
+    content: {
+      description: null,
+      _: {
+        id: {
+          description: "Identifier of the created offer",
+        },
       },
     },
   },

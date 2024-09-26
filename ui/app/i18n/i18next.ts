1ef1851ab0560ecadb316595d577cf7d5222e1b0
Samir BENFARES <sbenfares@gmail.com>
2024-09-26 17:06:52 +0200

    feat: traduction de la modal de login

--- ui/app/i18n/i18next.ts
+++ ui/app/i18n/i18next.ts
@@ -6,6 +6,7 @@ import type nsZod from "zod-i18n-map/locales/fr/zod.json";
 import type nsDocumentationTechnique from "./locales/fr/documentation-technique.json";
 import type nsExplorer from "./locales/fr/explorer.json";
 import type nsGlobal from "./locales/fr/global.json";
+import type nsInscriptionConnexion from "./locales/fr/inscription-connexion.json";
 import type { fallbackLng } from "./settings";
 
 declare module "i18next" {
@@ -16,6 +17,7 @@ declare module "i18next" {
       "documentation-technique": typeof nsDocumentationTechnique;
       explorer: typeof nsExplorer;
       global: typeof nsGlobal;
+      "inscription-connexion": typeof nsInscriptionConnexion;
       zod: typeof nsZod;
     };
   }

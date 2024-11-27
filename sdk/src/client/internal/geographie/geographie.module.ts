61f02dcc72159fb5fdee3683313641c1d83e1f75
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-11-27 21:46:25 +0100

    feat: add cache to certification, geographie and organisme module

--- sdk/src/client/internal/geographie/geographie.module.ts
+++ sdk/src/client/internal/geographie/geographie.module.ts
@@ -1,3 +1,6 @@
+import { LRUCache } from "lru-cache";
+import { stringify } from "safe-stable-stringify";
+
 import type { ICommune, IDepartement } from "../../../models/index.js";
 import { zCommune, zDepartement } from "../../../models/index.js";
 import type { IApiGetRoutes, IApiQuery } from "../../../routes/index.js";
@@ -10,13 +13,27 @@ export type GeographieModule = {
 };
 
 export function buildGeographieModule(apiClient: ApiClient): GeographieModule {
+  const communeCache = new LRUCache<string, ICommune[]>({
+    max: 500,
+    ttl: 1_000 * 60 * 60,
+  });
+
   return {
     rechercheCommune: async (
       querystring: IApiQuery<IApiGetRoutes["/geographie/v1/commune/search"]>
     ): Promise<ICommune[]> => {
+      const cacheKey = stringify(querystring);
+      const cached = communeCache.get(cacheKey);
+      if (cached) {
+        return cached;
+      }
+
       const data = await apiClient.get("/geographie/v1/commune/search", { querystring });
 
-      return parseApiResponse(data, zCommune.array());
+      const result = parseApiResponse(data, zCommune.array());
+      communeCache.set(cacheKey, result);
+
+      return result;
     },
     listDepartements: async (): Promise<IDepartement[]> => {
       const data = await apiClient.get("/geographie/v1/departement", {});

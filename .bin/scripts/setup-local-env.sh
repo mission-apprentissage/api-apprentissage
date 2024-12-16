32fe679abd12eb49ee80735965e154f36b1004f1
Samir BENFARES <sbenfares@gmail.com>
2024-12-16 12:14:50 +0100

    fix: win git bash specific

--- .bin/scripts/setup-local-env.sh
+++ .bin/scripts/setup-local-env.sh
@@ -10,24 +10,25 @@ delete_cleartext() {
 trap delete_cleartext EXIT
 "${SCRIPT_DIR}/get-vault-password-client.sh" > "${ROOT_DIR}/.vault_pwd.txt"
 
-docker run -it --rm \
+MSYS_NO_PATHCONV=1 docker run -it --rm \
   -v "${ROOT_DIR}:/root" \
   -e ANSIBLE_CONFIG="/root/.infra/ansible/ansible.cfg" \
-  alpine/ansible ansible all \
+  alpine/ansible sh -c 'chmod 0600 /root/.vault_pwd.txt && ansible all \
     --limit "local" \
     -m template \
     -a "src=\"/root/.infra/.env_server\" dest=\"/root/server/.env\"" \
     --extra-vars "@root/.infra/vault/vault.yml" \
-    --vault-password-file="/root/.vault_pwd.txt"
-docker run -it --rm \
+    --vault-password-file="/root/.vault_pwd.txt"'
+
+MSYS_NO_PATHCONV=1 docker run -it --rm \
   -v "${ROOT_DIR}:/root" \
   -e ANSIBLE_CONFIG="/root/.infra/ansible/ansible.cfg" \
-  alpine/ansible ansible all \
+  alpine/ansible sh -c 'chmod 0600 /root/.vault_pwd.txt && ansible all \
     --limit "local" \
     -m template \
     -a "src=\"/root/.infra/.env_ui\" dest=\"/root/ui/.env\"" \
     --extra-vars "@root/.infra/vault/vault.yml" \
-    --vault-password-file="/root/.vault_pwd.txt"
+    --vault-password-file="/root/.vault_pwd.txt"'
 
 echo "PUBLIC_VERSION=0-local" >> "${ROOT_DIR}/server/.env"
 echo "PUBLIC_PRODUCT_NAME=\"${PRODUCT_NAME}\"" >> "${ROOT_DIR}/server/.env"

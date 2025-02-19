6c157502709b5519eb607307c8e39868c74b791d
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-02-19 17:33:09 +0100

    fix: migration

--- server/src/migrations/20250219120014-cache_entreprise_validation.ts
+++ server/src/migrations/20250219120014-cache_entreprise_validation.ts
@@ -4,23 +4,31 @@ export const up = async () => {
   await getDbCollection("cache.entreprise").updateMany(
     {
       "data.type": "unite_legale",
+      "data.unite_legale": { $ne: null },
       "data.unite_legale.personne_physique_attributs.nom_naissance": null,
     },
     {
       $set: {
         "data.unite_legale.personne_physique_attributs.nom_naissance": null,
       },
+    },
+    {
+      bypassDocumentValidation: true,
     }
   );
   await getDbCollection("cache.entreprise").updateMany(
     {
       "data.type": "etablissement",
+      "data.etablissement": { $ne: null },
       "data.etablissement.unite_legale.personne_physique_attributs.nom_naissance": null,
     },
     {
       $set: {
         "data.etablissement.unite_legale.personne_physique_attributs.nom_naissance": null,
       },
+    },
+    {
+      bypassDocumentValidation: true,
     }
   );
 };

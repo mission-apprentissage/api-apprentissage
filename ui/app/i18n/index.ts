5aecc38556fcfb38854375f1900437d4520bfc21
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-06-13 14:42:59 +0200

    chore: fix typecheck

--- ui/app/i18n/index.ts
+++ ui/app/i18n/index.ts
@@ -1,5 +1,6 @@
 import acceptLanguage from "accept-language";
 import { createInstance } from "i18next";
+import type { TFunction, i18n as i18nT } from "i18next";
 import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next/initReactI18next";
 
@@ -8,7 +9,7 @@ import { getOptions } from "./settings";
 
 acceptLanguage.languages(["fr", "en"]);
 
-const initI18next = async (lang: Lang) => {
+const initI18next = async (lang: Lang): Promise<i18nT> => {
   const i18nInstance = createInstance();
   await i18nInstance
     .use(
@@ -25,10 +26,16 @@ const initI18next = async (lang: Lang) => {
   return i18nInstance;
 };
 
-export async function getServerTranslation<N extends Namespace>(lang: Lang, ns: N) {
+export async function getServerTranslation<N extends Namespace>(
+  lang: Lang,
+  ns: N
+): Promise<{
+  t: TFunction<N, undefined>;
+  i18n: i18nT;
+}> {
   const i18nextInstance = await initI18next(lang);
   return {
-    t: i18nextInstance.getFixedT<N>(lang, ns),
+    t: i18nextInstance.getFixedT<N, undefined, N>(lang, ns),
     i18n: i18nextInstance,
   };
 }

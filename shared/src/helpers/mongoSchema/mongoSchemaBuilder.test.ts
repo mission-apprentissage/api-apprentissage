a51ee593eede143738858271b4be2ee89a448dcb
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-06-28 01:17:44 +0200

    fix: tests

--- shared/src/helpers/mongoSchema/mongoSchemaBuilder.test.ts
+++ shared/src/helpers/mongoSchema/mongoSchemaBuilder.test.ts
@@ -1,12 +1,25 @@
 import { describe, expect, it } from "vitest";
 import { zodToMongoSchema } from "zod-mongodb-schema";
 
+import type { $ZodType, JSONSchema } from "zod/v4/core";
+import { zParisLocalDate } from "api-alternance-sdk/internal";
 import { modelDescriptors } from "../../models/models.js";
 
 describe("zodToMongoSchema", () => {
   modelDescriptors.forEach((descriptor) => {
     it(`should convert ${descriptor.collectionName} schema`, () => {
-      expect(zodToMongoSchema(descriptor.zod)).toMatchSnapshot();
+      expect(
+        zodToMongoSchema(descriptor.zod, (z: $ZodType): JSONSchema.Schema | null => {
+          if (z === zParisLocalDate) {
+            return {
+              type: "string",
+              format: "date-time",
+            };
+          }
+
+          return null;
+        })
+      ).toMatchSnapshot();
     });
   });
 });

c0da32e5dbcf568f73c50a23af2d9a1371d0fad2
Rémy Auricoste <remy.auricoste@gmail.com>
2025-07-03 10:54:06 +0200

    fix: maj de la documentation

--- sdk/src/docs/models/job/JobOfferExport.doc.ts
+++ sdk/src/docs/models/job/JobOfferExport.doc.ts
@@ -10,6 +10,9 @@ export const offerExportModelDoc = {
   properties: {
     url: {
       descriptions: [{ en: "URL of the export", fr: "URL de l'export" }],
+      examples: [
+        "https://s3.rbx.io.cloud.ovh.net/bucket/file.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD",
+      ],
     },
     lastUpdate: {
       descriptions: [{ en: "Export date", fr: "Date de l'export" }],

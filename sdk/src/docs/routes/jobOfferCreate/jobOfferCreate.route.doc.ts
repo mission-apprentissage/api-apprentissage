d489a8a3711e1a0fbbe4ae190565fee282872b42
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2025-07-29 16:55:52 +0200

    feat: add job publish rate limit

--- sdk/src/docs/routes/jobOfferCreate/jobOfferCreate.route.doc.ts
+++ sdk/src/docs/routes/jobOfferCreate/jobOfferCreate.route.doc.ts
@@ -4,8 +4,8 @@ import type { DocRoute } from "../../types.js";
 export const jobOfferCreateRouteDoc = {
   summary: depotOffrePageSummaryDoc.title,
   description: {
-    en: "This API allows you to maximize the visibility of your apprenticeship job offers by sharing them with the La bonne alternance service, which then automatically redistributes them to the most relevant candidates on its site as well as its partner sites.",
-    fr: "Cette API vous permet de maximiser la visibilité de vos offres d'emploi en alternance en les partageant avec le service La bonne alternance qui les redistribue automatiquement aux candidats les plus pertinents sur son site ainsi que sur ses sites partenaires.",
+    en: "This API allows you to maximize the visibility of your apprenticeship job offers by sharing them with the La bonne alternance service, which then automatically redistributes them to the most relevant candidates on its site as well as its partner sites. <br /><br /> Limit: 1 call per second",
+    fr: `Cette API vous permet de maximiser la visibilité de vos offres d'emploi en alternance en les partageant avec le service La bonne alternance qui les redistribue automatiquement aux candidats les plus pertinents sur son site ainsi que sur ses sites partenaires. <br /><br /> Limite : 1 appel par seconde`,
   },
   body: {
     description: null,

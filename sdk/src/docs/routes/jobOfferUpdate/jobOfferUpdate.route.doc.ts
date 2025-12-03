2c8906b750eb8c7ca8e7b0c4059f8e1e9cbc16f7
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-12-03 17:20:46 +0100

    fix: wording pour cohérence

--- sdk/src/docs/routes/jobOfferUpdate/jobOfferUpdate.route.doc.ts
+++ sdk/src/docs/routes/jobOfferUpdate/jobOfferUpdate.route.doc.ts
@@ -1,9 +1,9 @@
 import type { DocRoute } from "../../types.js";
 
 export const jobOfferUpdateRouteDoc = {
-  summary: { en: "Modify a work-study job offer.", fr: "Modifier une offre d'emploi en alternance." },
+  summary: { en: "Modify an apprenticeship job offer.", fr: "Modifier une offre d'emploi en alternance." },
   description: {
-    en: "This API allows you to maximize the visibility of your work-study job offers by sharing them with the service La bonne alternance, which then automatically distributes them closer to candidates on its site and its partner sites.",
+    en: "This API allows you to maximize the visibility of your apprenticeship job offers by sharing them with the service La bonne alternance, which then automatically distributes them closer to candidates on its site and its partner sites.",
     fr: "Cette API vous permet de maximiser la visibilité de vos offres d'emploi en alternance en les partageant avec le service La bonne alternance qui les redistribue automatiquement aux candidats les plus pertinents sur son site ainsi que sur ses sites partenaires.",
   },
   parameters: {

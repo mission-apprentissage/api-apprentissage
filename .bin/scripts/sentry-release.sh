e5eba51b2b87a2f3027894bf35a4b72235dec818
mathieu <mathieu.borrell@beta.gouv.fr>
2026-01-19 09:06:21 +0100

    feat: migration d'Ansible Vault vers SOPS

--- .bin/scripts/sentry-release.sh
+++ .bin/scripts/sentry-release.sh
@@ -7,16 +7,8 @@ VERSION=${1:?"Veuillez préciser la version à build"}
 COMMIT_ID=$(git rev-parse --short HEAD)
 PREV_COMMIT_ID=$(git rev-parse --short HEAD^1)
 
-if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE:-}" ]]; then
-  ansible_extra_opts+=("--vault-password-file" "${SCRIPT_DIR}/get-vault-password-client.sh")
-else
-  echo "Récupération de la passphrase depuis l'environnement variable ANSIBLE_VAULT_PASSWORD_FILE" 
-fi
-
-readonly VAULT_FILE="${ROOT_DIR}/.infra/vault/vault.yml"
-
-SENTRY_DSN=$(ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" | yq -r '.vault.SERVER_SENTRY_DSN')
-SENTRY_AUTH_TOKEN=$(ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" | yq -r '.vault.SENTRY_AUTH_TOKEN')
+SENTRY_DSN=$(sops --decrypt --extract '["SERVER_SENTRY_DSN"]' .infra/env.global.yml)
+SENTRY_AUTH_TOKEN=$(sops --decrypt --extract '["SENTRY_AUTH_TOKEN"]' .infra/env.global.yml)
 
 docker run \
   --platform=linux/amd64 \

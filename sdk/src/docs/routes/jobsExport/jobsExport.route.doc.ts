4b0bd80ba4a2510d2ce18c909262b0a237ea1269
Rémy Auricoste <remy.auricoste@gmail.com>
2026-02-17 10:53:10 +0100

    fix(lbac-3468): maj documentation endpoint /job/v1/export

--- sdk/src/docs/routes/jobsExport/jobsExport.route.doc.ts
+++ sdk/src/docs/routes/jobsExport/jobsExport.route.doc.ts
@@ -3,10 +3,10 @@ import type { DocRoute } from "../../types.js";
 export const jobsExportRouteDoc = {
   summary: { fr: "Export des offres d'emploi", en: "Export of the job offers" },
   description: {
-    en: `Displays all job offers.
-<br/>Offers are updated once a day at 3 AM Paris time.`,
-    fr: `Expose la totalité des offres d'emploi.
-<br/>Les offres sont mises à jour une fois par jour à 3h du matin heure de Paris.`,
+    en: `Lists all job opportunities (job postings and companies to which you can send unsolicited applications).
+<br/>Opportunities are updated once a day at 3:00 AM Paris time.`,
+    fr: `Expose la totalité des opportunités d'emploi (offres et entreprises auprès desquelles adresser des candidatures spontanées).
+<br/>Les opportunités sont mises à jour une fois par jour à 3h du matin heure de Paris.`,
   },
   response: {
     description: { en: "Success", fr: "Succès" },

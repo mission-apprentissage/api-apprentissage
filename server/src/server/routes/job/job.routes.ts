b4303d43a09b9b6ec1924b4892cb3a91dbaa6023
kevbarns <kevin.barnoin@ext.beta.gouv.fr>
2026-05-22 10:57:47 +0200

    fix: timeout

--- server/src/server/routes/job/job.routes.ts
+++ server/src/server/routes/job/job.routes.ts
@@ -161,7 +161,6 @@ export const jobRoutes = ({ server }: { server: Server }) => {
           endpoint: config.api.lba.endpoint,
           path: `/v3/jobs/export`,
           requestInit: { method: "GET" },
-          timeoutMs: 60_000,
         },
         response,
         { user, organisation: request.organisation ?? null }
